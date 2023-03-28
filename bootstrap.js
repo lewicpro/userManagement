be6f2ec26415b06367c9417c23a494c65a867573
Lewis Wanjara <lewis.wanjara@simu.iq>
2023-03-28 17:25:26 +0300

    first commit

---
+++ bootstrap.js
@@ -0,0 +1,23 @@
+﻿$.when(capabilitiesPromiseLoaded).then(function () {    
+   
+});
+
+var initcapabilities = function () {
+
+    if (datasource == "") {
+        if (window.location.hostname == "localhost") {
+            if (!using_explorer) {
+                datasource = window.location.protocol + "//192.168.0.134:7001";
+            } else {
+                datasource = window.location.protocol + "//192.168.0.134:7001";
+
+            }
+        } else {
+            if (using_explorer) {
+                analyzer_tcp_port = window.location.port;
+            }
+            datasource = window.location.protocol + "//" + window.location.hostname + ":" + analyzer_tcp_port;
+        }
+    }
+    
+}();
